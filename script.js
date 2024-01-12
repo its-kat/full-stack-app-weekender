949675566361c12686edea93659cd97c07f75031
its-kat <khalsell83@gmail.com>
2024-01-12 11:41:27 -0800

    added template literals

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function calcFactAge(year) {
   const age = currentYear - year;
 
   if (age >= 0) return age;
-  else return "Impossible year";
+  else return `Impossible year. Year needs to be less or equal  ${currentYear}`;
 }
 
 const age1 = calcFactAge(1983);
@@ -71,3 +71,15 @@ const message =
     ? "The fact is true"
     : "Might be false, check more sources...";
 // alert(message);
+
+const text = "Lisbon is the capital of Portugal";
+
+const upperText = text.toUpperCase();
+console.log(upperText);
+
+const str = `The current fact is "${text}". It is ${calcFactAge(
+  2015
+)} years old. It is probably ${
+  totalUpvotes > votesFalse ? "correct" : "not true"
+}.`;
+console.log(str);
