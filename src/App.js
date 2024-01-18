3a62b9567c085c882f2fe4bc53a743c978613c23
its-kat <khalsell83@gmail.com>
2024-01-17 19:51:57 -0800

    added interface components skeleton framework

--- src/App.js
+++ src/App.js
@@ -7,16 +7,45 @@
 import "./style.css";
 
 function App() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+  const appTitle = "Today I Learned";
 
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a Fact</button>
-    </header>
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a Fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form </form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
