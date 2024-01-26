8cceeea28a02fe67064d2a8b4d400fa9d1b023de
its-kat <khalsell83@gmail.com>
2024-01-25 18:30:21 -0800

    added useStates to fact form

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
 
   return (
     <>
-     <Header showForm={showForm} setShowForm={setShowForm} />
+      <Header showForm={showForm} setShowForm={setShowForm} />
       {showForm ? <NewFactForm /> : null}
 
       <main className="main">
@@ -70,12 +70,7 @@ function Header({ showForm, setShowForm }) {
   return (
     <header className="header">
       <div className="logo">
-        <img
-          src="logo.png"
-          height="68"
-          width="68"
-          alt="Today I Learned Logo"
-        />
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
 
         <h1>{appTitle}</h1>
       </div>
@@ -86,11 +81,7 @@ function Header({ showForm, setShowForm }) {
         {showForm ? "Close" : "Share a Fact"}
       </button>
     </header>
-  )
-}
-
-function NewFactForm() {
-  return <form className="fact-form">Fact form </form>;
+  );
 }
 
 const CATEGORIES = [
@@ -103,6 +94,45 @@ const CATEGORIES = [
   { name: "history", color: "#f97316" },
   { name: "news", color: "#8b5cf6" },
 ];
+
+function NewFactForm() {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        value={source}
+        type="text"
+        placeholder="Trustworthy source..."
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
 
 function CategoryFilter() {
   return (
@@ -149,7 +179,12 @@ function Fact({ fact }) {
     <li className="fact">
       <p>
         {fact.text}
-        <a className="source" href={fact.source} target="_blank">
+        <a
+          className="source"
+          href={fact.source}
+          target="_blank"
+          rel="noreferrer"
+        >
           (Source)
         </a>
       </p>
