0e25d9041fb472a557f5df857a525e8a07f2ba64
its-kat <khalsell83@gmail.com>
2024-01-29 14:22:06 -0800

    final touches and ready for build

--- src/App.js
+++ src/App.js
@@ -282,6 +282,9 @@ function FactList({ facts, setFacts }) {
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
 
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
+
   async function handleVote(columnName) {
     setIsUpdating(true);
 
@@ -303,6 +306,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⛔ DISPUTED]</span> : null}
         {fact.text}
         <a
           className="source"
